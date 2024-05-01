78336d1af1d511304209c1d49838eaa0f4f19e1b
Pei-chun, Tsai <71477465+pei9223@users.noreply.github.com>
2024-05-01 11:49:44 +0000

    backend-port 3333

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8888',
+        target: 'http://localhost:3333',
         changeOrigin: true
       }
     }
